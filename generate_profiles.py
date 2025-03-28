43973aefe29fc7a5f40fd0d22326147d4d1c8b27
Robert OGrady <turnstoashes350@gmail.com>
2025-03-28 08:18:19 -0400

    script to generate random profile data, started template for ranked recommendations

--- generate_profiles.py
+++ generate_profiles.py
@@ -1,18 +1,38 @@
 #file to generate a large number of profile data using Faker Python library
 import random
 from faker import Faker
-from service import profile_collection
+from service import profile_collection, hash_password
 
+#library with built in datasets containing real-world values and functions to select from relevant datasets
 fake = Faker()
 
-#function to generate profiles with random data
-def generate_profile_data():
-    
+#opitions for selections
+genders = ["male", "female", "nonbinary", "transgender", "other"]
+politics = ["conservative", "moderate", "liberal", "not", "other"]
+religions = ["agnostic", "atheist", "buddhist", "catholic", "christian", "hindu", "jewish", "muslim", "spiritual", "other", "not"]
+children = ["want", "open", "unsure", "dont", "not"]
+have_children = ["yes", "no", "not"]
 
-    return{}
+#Function to generate profiles with random data. Use Faker for generation and random for random selection from options
+def generate_profile_data():
+    return{
+        "username": fake.user_name(),
+        "password": hash_password("Robert88"), #use same password for all users for ease of testing
+        "email": fake.email(),
+        "first": fake.first_name(),
+        "last": fake.last_name(),
+        "age": random.randint(18, 65), #random age between 18 and 65
+        "occupation": fake.job(),
+        "gender": random.choice(genders),
+        "matchPreferences": random.sample(genders, k=random.randint(1, 3)), #randomly select how many genders user wants to match with
+        "politics": random.choice(politics),
+        "religion": random.choice(religions),
+        "wantChildren": random.choice(children),
+        "haveChildren": random.choice(have_children),
+    }
 
 #instert 50 generated profiles into Mongo db
 for _ in range(50):
     profile_collection.insert_one(generate_profile_data())
 
-print("Sample profiles added.")
+print("Sample profiles added.") #confirm function ran
---
+++ recommendations.py
@@ -0,0 +1,19 @@
+#score weighted filtering methods
+
+#calculate level of compatability
+def calculate_compatibility(user, recommended_user):
+    score = 0 #set score to 0 initially
+
+    #increase score for closer age
+
+
+    #increase score for shared preferences
+
+
+    return score
+
+#use score to return list of recommendations
+def get_ranked_recommendations(user_id):
+
+
+    return
